bc93e7f60892c36a9532811365b10caffed7c5e9
oxnut134 <oka70301@gmail.com>
2026-03-12 13:39:12 +0900

    Add migration route to web.php

--- backend/src/routes/web.php
+++ backend/src/routes/web.php
@@ -7,3 +7,15 @@ Route::get('/', function () {
 });
 
 require __DIR__.'/auth.php';
+
+
+// routes/web.php に追記
+Route::get('/run-migrate', function () {
+    try {
+        // \Illuminate\Support\Facades\Artisan を直接指定
+        \Illuminate\Support\Facades\Artisan::call('migrate', ['--force' => true]);
+        return "Migration success:<br><pre>" . \Illuminate\Support\Facades\Artisan::output() . "</pre>";
+    } catch (\Exception $e) {
+        return "Error: " . $e->getMessage();
+    }
+});
