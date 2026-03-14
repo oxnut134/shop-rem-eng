6d89348d7c75f28662d387ce664b997153a0dc4e
oxnut134 <oka70301@gmail.com>
2026-03-14 09:30:29 +0900

    update cords.php(backend)

--- backend/src/config/cors.php
+++ backend/src/config/cors.php
@@ -20,7 +20,7 @@ return [
     'allowed_methods' => ['*'],
 
     'allowed_origins' => [
-        'https://fluffy-cat-c8dedb.netlify.app',
+        'https://shop-rem-eng.net',
         'http://localhost:3000',
         'http://127.0.0.1:3000',
         'http://192.168.3.3:3000', // 
